ccbefac90d15a9aa374ffd19cb5a34aec050a91b
Max Guzman <maximiliano.guzman@walmart.com>
2019-01-29 12:08:17 -0300

    fix(terraform): add route table association with the subnet provided.

--- main.tf
+++ main.tf
@@ -2,11 +2,22 @@ data "azurerm_resource_group" "main" {
   name = "${var.resource_group}"
 }
 
+data "azurerm_subnet" "subnet" {
+  name                 = "${var.subnet_name}"
+  virtual_network_name = "${var.vnet_name}"
+  resource_group_name  = "${data.azurerm_resource_group.main.name}"
+}
+
 resource "azurerm_route_table" "route_table" {
-  name                          = "${var.route_table_name}-${var.cluster_name}-${var.environment}-${var.name_suffix}"
+  name                          = "${var.route_table_name}-${var.environment}-${data.azurerm_resource_group.main.location}-${var.name_suffix}"
   location                      = "${data.azurerm_resource_group.main.location}"
   resource_group_name           = "${data.azurerm_resource_group.main.name}"
   disable_bgp_route_propagation = "${var.disable_bgp_route_propagation}"
+}
+
+resource "azurerm_subnet_route_table_association" "route_table_association" {
+  subnet_id      = "${data.azurerm_subnet.subnet.id}"
+  route_table_id = "${azurerm_route_table.route_table.id}"
 }
 
 resource "azurerm_route" "route" {
--- variables.tf
+++ variables.tf
@@ -2,15 +2,19 @@ variable "resource_group" {
   type = "string"
 }
 
-variable "cluster_name" {
-  type = "string"
-}
-
 variable "environment" {
   type = "string"
 }
 
 variable "name_suffix" {
+  type = "string"
+}
+
+variable "vnet_name" {
+  type = "string"
+}
+
+variable "subnet_name" {
   type = "string"
 }
 
